1161e6ee78b2c13b1ddc090ed640bb42e423e38b
jaeesu <wogmlsu@gmail.com>
2022-01-11 17:24:24 +0900

    feat: 질문글 리스트 테이블 추가

--- src/router.js
+++ src/router.js
@@ -1,14 +1,16 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
-import Ranking from "./components/views/Ranking.vue"
+import Ranking from "./components/views/Ranking.vue";
+import Question from "./components/views/Question.vue";
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-    mode: "history",
-    routes: [
-        {path: '/',component: Ranking}
-    ]
-})
+  mode: "history",
+  routes: [
+    { path: "/", component: Ranking },
+    { path: "/question", component: Question },
+  ],
+});
 export default router;
