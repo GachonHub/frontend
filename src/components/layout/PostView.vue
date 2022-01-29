b2aecdb88ec6f9a4bc16b207c83041cff0bff65d
jaeesu <wogmlsu@gmail.com>
2022-01-29 23:58:19 +0900

    feat: 질문글 view 추가

---
+++ src/components/layout/PostView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        {{$route.params.id}}
+        <div id="title">
+            {{title}}
+        </div>
+        <hr>
+        <div id="post-info">
+            {{author}}
+            {{date}}
+        </div>
+
+        <div id="content">
+            {{content}}
+        </div>
+        <hr>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name : "post-view",
+    props : {
+        title : String,
+        author : String,
+        date : String,
+        content : String
+    }
+}
+</script>
+
+<style scoped>
+
+
+hr {
+  margin: 0;
+  border: 0;
+  border-top: 1px solid #b2b2b2;
+}
+
+
+</style>
