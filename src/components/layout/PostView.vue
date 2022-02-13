09f32c16c310af5cfbe4a760158c71a563fc1551
jaeesu <wogmlsu@gmail.com>
2022-02-14 00:04:47 +0900

    feat: 질문글 view

--- src/components/layout/PostView.vue
+++ src/components/layout/PostView.vue
@@ -1,15 +1,40 @@
 <template>
     <div>
-        <!-- {{$route.params.id}} -->
         <div id="title">{{question.title}}</div>
-        <hr>
         <div id="post-info">
-            {{question.author}}
-            {{question.date}}
+            <table>
+                <td></td>
+                <td id="wth80">작성자</td>
+                <td id="wth100">{{question.author}}</td>
+                <td id="wth80">작성일</td>
+                <td id="wth100">{{question.date}}</td>
+            </table>
+        </div>
+        <div id="content">
+            {{question.content}}
+            <img class="p-4" :src="question.img" alt="">
         </div>
         <hr>
-        <div id="content">{{question.content}}</div>
-        <hr>
+        <template v-for="item in reply" :key="item">
+        <!-- 문의 사항 댓글 -->
+        <!-- <div class="form-control" id="reply" style="background-color:#EBEDF0;">
+            <div id="reply-title">
+                re: {{question.title}} {{reply.date}}
+            </div>
+            <div id="reply-content">
+                {{reply.content}}
+            </div>
+        </div> -->
+            <div class="form-control" :id="(item.author === question.author) ? 'reply1' : 'reply'">
+                <div id="reply-title">
+                    {{item.author}}&nbsp;&nbsp;&nbsp;{{item.date}}
+                </div>
+                <div id="reply-content">
+                    {{item.content}}
+                </div>
+            </div>
+        </template>
+
 
     </div>
 </template>
@@ -18,7 +43,8 @@
 export default {
     name : "post-view",
     props : {
-        question : Array
+        question : Array,
+        reply: Array,
     }
 }
 </script>
@@ -37,6 +63,26 @@ hr {
     text-align: center;
 }
 
+table {
+    border-top: 0.5px solid #b2b2b2;
+    border-bottom: 0.5px solid #b2b2b2;
+    text-align: center;
+}
+
+table td {
+    border: none;
+}
+
+#wth80 {
+    width: 80px;
+    border-left: 0.5px solid #b2b2b2;
+}
+
+#wth100 {
+    width: 100px;
+    border-left: 0.5px solid #b2b2b2;
+}
+
 #post-info {
     height: 50px;
     line-height: 50px;
@@ -44,9 +90,35 @@ hr {
 
 #content {
     min-height: 200px;
-    padding: 30px;
-    display: block;
+    padding: 50px;
 }
 
+.form-control {
+    font-size: 12px;
+    border: none;
+    margin-top: 10px;
+    margin-left: auto;
+    margin-right: auto;
+    width: 800px;
+}
+
+#reply {
+    border: 2px solid #EBEDF0;
+    float: left;
+}
+
+#reply1 {
+    background-color: #EBEDF0;
+    float: right;
+}
+
+#reply-title {
+    padding: 10px;
+    padding-bottom: 0;
+}
+
+#reply-content {
+    padding: 10px;
+}
 
 </style>
