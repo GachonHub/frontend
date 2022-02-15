b5af05d7f8ba036c26f475758bebc34cecbf59c0
jaeesu <wogmlsu@gmail.com>
2022-02-15 14:22:17 +0900

    style

--- src/components/views/Ranking.vue
+++ src/components/views/Ranking.vue
@@ -254,7 +254,8 @@ export default {
 
 <style scoped>
 .container {
-    width: 1200px;
+    max-width: 1200px;
+    min-width: 1200px;
     margin: 0 auto;
     margin-bottom: 20px;
 }
--- src/components/views/Test.vue
+++ src/components/views/Test.vue
@@ -1,26 +1,24 @@
 <template>
     <div>
-        <table>
+        <table cellspacing="0" cellpadding="0">
             <tr>
-                <th id="head">none</th>
-                <th id="head">mon</th>
-                <th id="head">tue</th>
-                <th id="head">wed</th>
-                <th id="head">thu</th>
-                <th id="head">fri</th>
-                <th id="head">sat</th>
-                <th id="head">sun</th>
+                <th id="head" v-for="item in week" :key="item">
+                    <div id="div-th">{{item}}</div>
+                </th>
+                
             </tr>
             
             <template v-for="item in month">
                 <tr v-for="(index,data) in parseInt(item.date)" :key="data">
-                    <td v-for="idx in 7" :key="idx">{{idx}}</td>
-                    <th id="head" v-if="index==1" :rowspan="parseInt(item.date)">jan</th>
+                    <td class="items" v-for="idx in 7" :key="idx">
+                        <div id="div-td">{{idx}}</div>
+                    </td>
+                    <th class="head1" style="vertical-align:top;" v-if="index==1" :rowspan="parseInt(item.date)">
+                        <div id="head" style="width:20px; height:20px;">jan</div>
+                    </th>
                 </tr>
 
             </template>
-
-
 
             <!-- month, month.name month.date -->
         </table>
@@ -31,6 +29,7 @@
 export default {
     data() {
         return {
+            week : ["SUN", "SAT", "FRI", "THU", "WED", "THU", "MON", " "],
             month : [
                 {
                     name : 'jan',
@@ -64,13 +63,51 @@ export default {
 
 <style scoped>
 table {
-    width: 300px;
-    -webkit-transform: rotate(90deg); /* Chrome, Safari, Opera */
+    width: 150px;
+    height: 500px;
     transform: rotate(-90deg);
+    font-size: 5px;
+    border: 0;
+    margin: 100px auto auto 500px;
 }
 
 #head {
-    -webkit-transform: rotate(90deg); /* Chrome, Safari, Opera */
-    transform: rotate(90deg);
+    writing-mode: vertical-rl;
+    border: 0;
 }
+
+.items {
+    background: #EBEDF0;
+    border: 2px solid #DFE1E4;
+    line-height: 15px;
+    width: 15px;
+    height: 15px;
+    font-size: 5px;
+}
+
+#div-td {
+    width: 15px;
+    height: 15px;
+    margin: 0;
+    padding: 0;
+}
+
+#div-th {
+    font-size: 5px;
+    width: 15px;
+    margin: 0;
+}
+
+table tr th {
+    line-height: 20px;
+    font-size: 5px;
+    border: 0;
+}
+
+#head1 {
+    height: 20px;
+}
+
+
+
 </style>
