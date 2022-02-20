82b836bbcb9ad3c65d46fc860ef40c1cbcf8aa21
jaeesu <wogmlsu@gmail.com>
2022-02-20 23:55:00 +0900

    fix: 버튼 움직임 수정

---
+++ src/components/layout/common/switch.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <input id="checkbox" type="checkbox" @change="check">
+        <label for="checkbox">
+            <div id="switch">
+                <div id="sphere"></div>
+            </div>
+        </label>
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: {
+        recruit:Boolean
+    },
+    methods: {
+        check() {
+            var recruit = document.getElementById("checkbox").checked;
+            if (recruit) {
+                document.getElementById("switch").style.background="#75b798";
+                document.getElementById("sphere").style.transform = "translate(17px)";
+            }
+            // on
+            else {
+                document.getElementById("switch").style.background="lightgray";
+                document.getElementById("sphere").style.transform = "translate(0px)";
+
+            }
+            // off
+        }
+    },
+    mounted() {
+        this.check(this.recruit);
+    }
+}
+</script>
+
+<style scoped>
+#checkbox {
+    display: none;
+}
+
+#switch {
+    width: 35px;
+    height: 18px;
+    border-radius: 20px;
+    background-color: lightgray;
+    box-shadow: inset 1px 5px 1px rgba(0,0,0,0.25);
+}
+#sphere {
+    width: 10px;
+    height: 10px;
+    border-radius: 70%;
+    background: white;
+    position:relative;
+    top: 4px;
+    left: 4px;
+}
+</style>
